c61f8367a8a7c47da43e8634cc1bd4349b8bbff4
aryaminus <sunim.54@gmail.com>
2020-08-09 10:32:36 +0545

    FIX #2 🚀 Deploy to Heroku

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::net::{TcpListener, TcpStream};
 
 // localhost
 const MSG_SIZE: usize = 32;
-const LOCAL: &str = "127.0.0.1:6000";
+const LOCAL: &str = "https://protected-everglades-84717.herokuapp.com";
 
 fn thread_sleep() {
     thread::sleep(std::time::Duration::from_millis(120));
